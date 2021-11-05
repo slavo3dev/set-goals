4dee6d65e12f3c3d45f5d8dba52b0a4fb86c3669
slavo3dev <slavo@slavo3.com>
2021-11-04 20:43:24 -0400

    adding button

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button} from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Button title="Add Golas" />
       <Text style={styles.text}>Prototype.Next</Text>
       <Text style={styles.text}>Start Building Project</Text>
       <StatusBar style="auto" />
